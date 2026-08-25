6738f56c634442c49023751eb8437c8682381596
thaisgilkrog <thaisgilkrog@fedora.home>
2026-08-25 09:48:21 +0200

    added more ranks

--- app/Support/UserRank.php
+++ app/Support/UserRank.php
@@ -7,11 +7,14 @@ class UserRank
     public static function fromCount(int $count): string
     {
         return match (true) {
-            $count >= 100 => 'VAULT MASTER',
-            $count >= 50  => 'COLLECTOR',
-            $count >= 20  => 'CURATOR',
-            $count >= 5   => 'ARCHIVIST',
-            default       => 'INITIATE',
+            $count >= 1000 => 'VAULT LEGEND',
+            $count >= 500  => 'VAULT SOVEREIGN',
+            $count >= 250  => 'VAULT MASTER',
+            $count >= 100  => 'VAULT KEEPER',
+            $count >= 50   => 'COLLECTOR',
+            $count >= 20   => 'CURATOR',
+            $count >= 5    => 'ARCHIVIST',
+            default        => 'INITIATE',
         };
     }
 }
